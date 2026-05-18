152816072d496e42184a525c3ec613576e58a1ef
Manthan Bhosale <cryptomb2006@gmail.com>
2026-05-18 19:50:58 +0530

    Update download URL to point to public repo

--- Casks/entervoid.rb
+++ Casks/entervoid.rb
@@ -2,7 +2,7 @@ cask "entervoid" do
   version "1.0.0"
   sha256 "f14990a65c22271454cc097f3e604dd070cde1525f396a4bee59d1b2fd244bc1"
 
-  url "https://github.com/MSB-io/Void/releases/download/v#{version}/Void-#{version}.zip"
+  url "https://github.com/MSB-io/homebrew-void/releases/download/v#{version}/Void-#{version}.zip"
   name "Void"
   desc "Unlimited encrypted cloud storage for macOS"
   homepage "https://entervoid.me"
